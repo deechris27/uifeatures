41423a51f057d3588c120c1e2ea0e1124c6f733e
Deepak <dd43028@gmail.com>
2020-05-16 20:09:07 +0530

    build

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/">
      <App />
     </HashRouter>
   </React.StrictMode>,
